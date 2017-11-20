e2031ae063756492e3808cce4f26b2f65a603a0c
LAPTOP-09QGLT5H\Geraldo <geraldo.rodriguesveloso111@gmail.com>
2017-11-20 13:25:38 +0000

    Commit, la cosa va de diseño

--- PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/MainActivity.java
+++ PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent tabActivity = new Intent(getApplicationContext(), TabActivity.class);
         startActivity(tabActivity);
-        
+
+
     }
 }
---
+++ PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/Statistic.java
@@ -0,0 +1,93 @@
+package gs1.gestorsm.prototipegs1;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Statistic {
+
+    /**
+     * Default constructor
+     */
+    public Statistic() {
+    }
+
+    public int getViewedMovies() {
+        return viewedMovies;
+    }
+
+    public int getSeriesCompleted() {
+        return seriesCompleted;
+    }
+
+    public int getViewedChapters() {
+        return viewedChapters;
+    }
+
+    public float getHoursViewed() {
+        return hoursViewed;
+    }
+
+    public int getRecommendations() {
+        return recommendations;
+    }
+
+    public float getContentAverageScore() {
+        return contentAverageScore;
+    }
+
+    public float getAverageMonthlySeries() {
+        return averageMonthlySeries;
+    }
+
+    public float getAverageMonthlyMovies() {
+        return averageMonthlyMovies;
+    }
+
+    /**
+     * 
+     */
+
+    private int viewedMovies;
+
+    /**
+     * 
+     */
+    private int seriesCompleted;
+
+    /**
+     * 
+     */
+    private int viewedChapters;
+
+    /**
+     * 
+     */
+    private float hoursViewed;
+
+    /**
+     * 
+     */
+    private int recommendations;
+
+    /**
+     * 
+     */
+    private float contentAverageScore;
+
+    /**
+     * 
+     */
+    private float averageMonthlySeries;
+
+    /**
+     * 
+     */
+    private float averageMonthlyMovies;
+
+
+
+
+
+}
