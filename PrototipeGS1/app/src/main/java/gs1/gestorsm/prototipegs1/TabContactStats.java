82f075aab2e2b75505471ff8b56cae4f7fd6bf5b
LAPTOP-09QGLT5H\Geraldo <geraldo.rodriguesveloso111@gmail.com>
2017-11-21 12:28:49 +0000

    Commit contacts Stat casi

--- PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/TabContactStats.java
+++ PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/TabContactStats.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Geraldo on 20/11/2017.
@@ -15,6 +19,20 @@ public class TabContactStats extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.contactstats_tab, container, false);
+        Statistic statistic = new Statistic();
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Recommendation: " + statistic.getRecommendations());
+        arrayList.add("Viewed Movies: " + statistic.getViewedMovies());
+        arrayList.add("Viewed Series: " + statistic.getViewedChapters());
+        arrayList.add("Monthly Movies: " + statistic.getAverageMonthlyMovies());
+        arrayList.add("Hours Viewed: " + statistic.getHoursViewed());
+        arrayList.add("Content Score: " + statistic.getContentAverageScore());
+        arrayList.add("Series Completed: " + statistic.getSeriesCompleted());
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_contact_stat, R.id.text_view_contact_stat, arrayList);
+        ListView listView = rootView.findViewById(R.id.list_view_contact_stats);
+        listView.setAdapter(adapter);
+
         return rootView;
     }
 }
