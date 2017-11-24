8e268a9ee92452a908926f1ecf63e964f840f099
DESKTOP-K2J3TU6\Jorge <jorge.fernandez107@alu.ulpgc.es>
2017-11-24 22:09:59 +0000

    conexionbd added

---
+++ PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/ConnectResponse.java
@@ -0,0 +1,12 @@
+package gs1.gestorsm.prototipegs1;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Jorge on 23/11/2017.
+ */
+
+public interface ConnectResponse {
+
+    void processFinish(String output, ArrayList<ArrayList<String>> datos);
+}
