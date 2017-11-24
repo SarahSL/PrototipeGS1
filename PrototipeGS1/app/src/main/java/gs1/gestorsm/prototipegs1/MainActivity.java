5a807c8b156be51e0082480d08784c1bf2267ded
SarahSL <you@example.com>
2017-11-25 00:50:00 +0100

    fixed some problems

--- PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/MainActivity.java
+++ PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/MainActivity.java
@@ -1,25 +1,37 @@
 package gs1.gestorsm.prototipegs1;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements ConnectResponse {
+
+    ArrayList<ArrayList<String>> datos = new ArrayList<>();
+    Connect con = new Connect();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        boolean sem = true;
-        if (sem) {
-            Intent tabActivity = new Intent(getApplicationContext(), TabActivity.class);
-            startActivity(tabActivity);
-            //System.out.println("as");
-        } else {
-            Intent tabActivity = new Intent(getApplicationContext(), ProfileActivity.class);
-            startActivity(tabActivity);
-        }
 
+        //con.setSql("Select * from user");
+        //Mode 0 - Select
+        //Mode 1 - Update y Delete
+        con.setSql("INSERT INTO `evaluation`(`cod_user`, `cod_content`, `score`) VALUES (2,2,2)", 1);
+        con.delegate = this;
+        con.Connect();
+    }
 
+    @Override
+    public void processFinish(String str, ArrayList<ArrayList<String>> datos) {
+        this.datos = datos;
+        //trabajo();
+    }
+
+    public void trabajo(){
+        TextView user = (TextView)findViewById(R.id.tv);
+        user.setText(datos.get(0).get(1));
     }
 }
