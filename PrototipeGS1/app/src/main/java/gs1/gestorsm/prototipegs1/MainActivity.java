4563b5311fa2a53e4bc3157afd4a3175a9c5091f
SarahSL <you@example.com>
2017-11-25 00:46:59 +0100

    rebase of geraldo

--- PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/MainActivity.java
+++ PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/MainActivity.java
@@ -1,37 +1,17 @@
 package gs1.gestorsm.prototipegs1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity implements ConnectResponse {
-
-    ArrayList<ArrayList<String>> datos = new ArrayList<>();
-    Connect con = new Connect();
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //con.setSql("Select * from user");
-        //Mode 0 - Select
-        //Mode 1 - Update y Delete
-        con.setSql("INSERT INTO `evaluation`(`cod_user`, `cod_content`, `score`) VALUES (2,2,2)", 1);
-        con.delegate = this;
-        con.Connect();
-    }
-
-    @Override
-    public void processFinish(String str, ArrayList<ArrayList<String>> datos) {
-        this.datos = datos;
-        //trabajo();
-    }
-
-    public void trabajo(){
-        TextView user = (TextView)findViewById(R.id.tv);
-        user.setText(datos.get(0).get(1));
+        Intent tabActivity = new Intent(getApplicationContext(), TabActivity.class);
+        startActivity(tabActivity);
+        
     }
 }
--- PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/TabMyStats.java
+++ PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/TabMyStats.java
@@ -1,8 +1,21 @@
 package gs1.gestorsm.prototipegs1;
 
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 /**
  * Created by Geraldo on 20/11/2017.
  */
 
-public class TabMyStats {
+public class TabMyStats extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.mystats_tab, container, false);
+        return rootView;
+    }
 }
