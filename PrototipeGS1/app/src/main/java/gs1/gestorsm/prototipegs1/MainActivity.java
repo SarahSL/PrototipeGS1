2dc39d4b7f231594a8f5483c10b289f891416b74
Tomás Pérez Márquez <topema_lp@hotmail.com>
2017-11-25 04:48:15 +0000

    commit gran parte funcionalidad filtrar por categorias

--- PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/MainActivity.java
+++ PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/MainActivity.java
@@ -1,12 +1,14 @@
 package gs1.gestorsm.prototipegs1;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements ConnectResponse {
+public class MainActivity extends AppCompatActivity  {
 
     ArrayList<ArrayList<String>> datos = new ArrayList<>();
     Connect con = new Connect();
@@ -15,15 +17,18 @@ public class MainActivity extends AppCompatActivity implements ConnectResponse {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this,filtraCategoria.class);
+        startActivity(intent);
 
-        //con.setSql("Select * from user");
+        /*//con.setSql("Select * from user");
         //Mode 0 - Select
         //Mode 1 - Update y Delete
         con.setSql("INSERT INTO `evaluation`(`cod_user`, `cod_content`, `score`) VALUES (2,2,2)", 1);
         con.delegate = this;
         con.Connect();
+*/
     }
-
+/*
     @Override
     public void processFinish(String str, ArrayList<ArrayList<String>> datos) {
         this.datos = datos;
@@ -33,5 +38,5 @@ public class MainActivity extends AppCompatActivity implements ConnectResponse {
     public void trabajo(){
         TextView user = (TextView)findViewById(R.id.tv);
         user.setText(datos.get(0).get(1));
-    }
+    }*/
 }
