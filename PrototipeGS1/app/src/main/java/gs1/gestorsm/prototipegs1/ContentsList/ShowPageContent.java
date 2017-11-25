53ab00f9fdcd82b6e11bbe234b4121240cab278e
SarahSL <you@example.com>
2017-11-25 15:47:45 +0000

    wip

--- PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/ContentsList/ShowPageContent.java
+++ PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/ContentsList/ShowPageContent.java
@@ -7,34 +7,45 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
+import gs1.gestorsm.prototipegs1.Connect;
+import gs1.gestorsm.prototipegs1.ConnectResponse;
 import gs1.gestorsm.prototipegs1.R;
 
 /**
  * Created by Javier on 25/11/2017.
  */
 
-public class ShowPageContent extends AppCompatActivity {
+public class ShowPageContent extends AppCompatActivity implements ConnectResponse{
     private SectionsPageAdapter mSectionsPageAdapter;
+    ArrayList<ArrayList<String>> datos = new ArrayList<>();
     private ViewPager mViewPager;
+    String idContent;
+    Connect con;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_page_show);
         Intent intent = getIntent();
-        String extra = intent.getStringExtra("aux");
-        System.out.println(extra);
+        idContent = intent.getStringExtra("aux");
+        System.out.println(idContent);
         TextView textView = findViewById(R.id.show_page_content_text);
-        textView.setText(extra);
+        textView.setText(idContent);
 
         mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById(R.id.container);
         setupViewPager(mViewPager);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
+
+        getContentImage();
     }
 
 
@@ -45,5 +56,17 @@ public class ShowPageContent extends AppCompatActivity {
         adapter.addFragment(new EpisodesFragment(),"Episodes");
         adapter.addFragment(new SocialFragment(),"Social");
         viewPager.setAdapter(adapter);
+    }
+    @Override
+    public void processFinish(String str, ArrayList<ArrayList<String>> datos) {
+        this.datos = datos;
+
+    }
+    private void getContentImage(){
+        con = new Connect();
+        // TO DO:  HAY QUE AÑADIR EL ID_USER
+        con.setSql("Select content.image from content where content.id_content="+idContent+"", 0);
+        con.delegate = this;
+        con.Connect();
     }
 }
