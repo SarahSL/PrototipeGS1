fd94767bc0b2c79323be37b8c522939e0135d401
SarahSL <you@example.com>
2017-11-25 12:24:53 +0000

    Show the content in the list done TODO- *put a image *click and go to the content page

--- PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/ContentsList/CompletedFragment.java
+++ PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/ContentsList/CompletedFragment.java
@@ -6,24 +6,82 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+
+import gs1.gestorsm.prototipegs1.Connect;
+import gs1.gestorsm.prototipegs1.ConnectResponse;
 import gs1.gestorsm.prototipegs1.R;
 
 /**
  * Created by Javier on 25/11/2017.
  */
 
-public class CompletedFragment extends Fragment {
+public class CompletedFragment extends Fragment implements ConnectResponse{
 
-
+    ArrayList<ArrayList<String>> datos = new ArrayList<>();
+    ArrayList<String> completedElements =new ArrayList<>();
+    Connect con;
+    View view;
+    ArrayAdapter<String> adapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_completed,container,false);
+        view = inflater.inflate(R.layout.fragment_completed,container,false);
         //HACER LA  MAGIA DEL ACTIVITY
-        TextView textView = view.findViewById(R.id.completed);
-        textView.setText("JSADOASDOJASDAS");
+        movieConsult();
+        serieConsult();
         return view;
+    }
+    @Override
+    public void processFinish(String str, ArrayList<ArrayList<String>> datos) {
+        this.datos = datos;
+        AddObjets();
+
+        // CREATE DE ADAPTER WITH ALL THE DATA FROM ALL THE CONSULTS
+        adapter=new ArrayAdapter<>(getActivity(),R.layout.fragment_completed,R.id.textView2
+                , completedElements);
+        ListView listView = view.findViewById(R.id.completed_list);
+
+        listView.setAdapter(adapter);
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        adapter.clear();
+    }
+    private void movieConsult(){
+        con = new Connect();
+        // TO DO:  HAY QUE AÑADIR EL ID_USER
+        con.setSql("SELECT content.title,contentType.name  "+
+                "FROM content,contentType "+
+                "INNER JOIN movie, viewList "+
+                "WHERE content.id_content = movie.cod_content "+
+                "AND viewList.cod_movie = movie.id_movie "+
+                "AND viewList.cod_user =1 AND contentType.id_contentType = content.cod_contentType", 0);
+        con.delegate = this;
+        con.Connect();
+    }
+    private void serieConsult(){
+        con = new Connect();
+        // TO DO:  HAY QUE AÑADIR EL ID_USER
+        con.setSql("SELECT content.title, contentType.name " +
+                "FROM content,contentType " +
+                "INNER JOIN serie,season,chapter, viewList " +
+                "WHERE content.id_content = serie.cod_content " +
+                "AND serie.id_serie=season.cod_serie " +
+                "AND season.id_season=chapter.cod_season " +
+                "AND viewList.cod_chapter = chapter.id_chapter " +
+                "AND viewList.cod_user =1 AND contentType.id_contentType=content.cod_contentType", 0);
+        con.delegate = this;
+        con.Connect();
+    }
+    private void AddObjets(){
+        for (int i = 0; i < datos.size(); i++) {
+            completedElements.add(datos.get(i).get(0)+ "\t\t" + datos.get(i).get(1));
+        }
+
     }
 }
--- PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/ContentsList/OnHoldFragment.java
+++ PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/ContentsList/OnHoldFragment.java
@@ -6,23 +6,82 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+
+import gs1.gestorsm.prototipegs1.Connect;
+import gs1.gestorsm.prototipegs1.ConnectResponse;
 import gs1.gestorsm.prototipegs1.R;
 
 /**
  * Created by Javier on 25/11/2017.
  */
 
-public class OnHoldFragment extends Fragment {
+public class OnHoldFragment extends Fragment implements ConnectResponse{
 
+    ArrayList<ArrayList<String>> datos = new ArrayList<>();
+    ArrayList<String> onHoldElements =new ArrayList<>();
+    Connect con;
+    View view;
+    ArrayAdapter<String> adapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_hold_on,container,false);
+        view = inflater.inflate(R.layout.fragment_hold_on,container,false);
         //HACER LA  MAGIA DEL ACTIVITY
-        TextView textView = view.findViewById(R.id.on_hold);
-        textView.setText("JSADOASDOJASDAS");
+        movieConsult();
+        serieConsult();
         return view;
+    }
+
+    @Override
+    public void processFinish(String str, ArrayList<ArrayList<String>> datos) {
+        this.datos = datos;
+        AddObjets();
+        // CREATE DE ADAPTER WITH ALL THE DATA FROM ALL THE CONSULTS
+        adapter = new ArrayAdapter<>(getActivity(),R.layout.fragment_hold_on,R.id.textView2
+                , onHoldElements);
+        ListView listView = view.findViewById(R.id.on_hold_list);
+        listView.setAdapter(adapter);
+
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        adapter.clear();
+    }
+    private void movieConsult(){
+        con = new Connect();
+        // TO DO:  HAY QUE AÑADIR EL ID_USER
+        con.setSql("SELECT content.title,contentType.name " +
+                "FROM content,contentType " +
+                "INNER JOIN movie, waitingList " +
+                "WHERE content.id_content = movie.cod_content " +
+                "AND waitingList.cod_movie = movie.id_movie " +
+                "AND waitingList.cod_user =1 AND contentType.id_contentType=content.cod_contentType ", 0);
+        con.delegate = this;
+        con.Connect();
+    }
+    private void serieConsult(){
+        con = new Connect();
+        // TO DO:  HAY QUE AÑADIR EL ID_USER
+        con.setSql("SELECT content.title,contentType.name "+
+                "FROM content,contentType "+
+                "INNER JOIN serie,season,chapter, waitingList "+
+                "WHERE content.id_content = serie.cod_content "+
+                "AND serie.id_serie=season.cod_serie "+
+                "AND season.id_season=chapter.cod_season "+
+                "AND waitingList.cod_chapter = chapter.id_chapter "+
+                "AND waitingList.cod_user =1 AND contentType.id_contentType=content.cod_contentType", 0);
+        con.delegate = this;
+        con.Connect();
+    }
+    private void AddObjets(){
+        for (int i = 0; i < datos.size(); i++) {
+            onHoldElements.add(datos.get(i).get(0)+ "\t\t" + datos.get(i).get(1));
+        }
+
     }
 }
