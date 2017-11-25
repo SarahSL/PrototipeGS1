aef2607c93b646a068c8d593d531ab59b2634bcf
Tomás Pérez Márquez <topema_lp@hotmail.com>
2017-11-25 15:56:34 +0000

    commit preventivo

--- PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/SearchContent.java
+++ PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/SearchContent.java
@@ -2,7 +2,11 @@ package gs1.gestorsm.prototipegs1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.SearchView.OnQueryTextListener;
@@ -10,6 +14,7 @@ import android.widget.Toast;
 import android.widget.Toolbar;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import static android.widget.SearchView.*;
 
@@ -20,11 +25,13 @@ import static android.widget.SearchView.*;
 public class SearchContent extends AppCompatActivity implements ConnectResponse {
 
     ArrayList<ArrayList<String>> datos = new ArrayList<>();
+    ArrayList<String> result = new ArrayList<>();
     Connect con = new Connect();
+    ArrayAdapter<String> adaptador;
 
-    ArrayList<String> res = new ArrayList<String>();
+    EditText texto;
+    Button search;
 
-    SearchView search;
     ListView results;
 
 
@@ -32,18 +39,71 @@ public class SearchContent extends AppCompatActivity implements ConnectResponse 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contentsearch);
 
-        search = findViewById(R.id.searchView);
+
+        texto = findViewById(R.id.searchText);
+        search = findViewById(R.id.searchButton);
         results = findViewById(R.id.searchResult);
+
+        adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,result);
+
+        results.setAdapter(adaptador);
+
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                llamada1();
+                llamada2();
+                llamada3();
+                rellenaLista();
+            }
+        });
     }
 
-    public boolean onQueryTextSubmit(String text) {
-        Toast.makeText(this, "Searching for " + text, Toast.LENGTH_LONG).show();
-        return false;
+    public void rellenaLista() {
+        Iterator<String> it =  result.iterator();
+        while(it.hasNext()){
+            adaptador.add(it.toString());
+        }
+    }
+
+    private void llamada3() {
+        con = new Connect();
+        con.setSql("SELECT content.title FROM platform WHERE content.title LIKE '%"+texto.getText()+"%'", 0);
+        con.delegate = this;
+        con.Connect();
+    }
+
+    private void llamada2() {
+        con = new Connect();
+        con.setSql("SELECT platform.name FROM platform WHERE platform.name LIKE '%"+texto.getText()+"%'", 0);
+        con.delegate = this;
+        con.Connect();
+    }
+
+    private void llamada1() {
+        con = new Connect();
+        con.setSql("SELECT cast.name FROM cast WHERE cast.name LIKE '%"+texto.getText()+"%'", 0);
+        con.delegate = this;
+        con.Connect();
     }
 
 
     @Override
     public void processFinish(String output, ArrayList<ArrayList<String>> datos) {
         this.datos = datos;
+        copy();
+    }
+
+    private void copy() {
+        for (ArrayList<String> list : datos)
+        {
+            for (String num : list)
+            {
+                result.add(num);
+                System.out.println(num);
+            }
+        }
+
     }
 }
--- PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/commentContent.java
+++ PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/commentContent.java
@@ -1,8 +1,35 @@
 package gs1.gestorsm.prototipegs1;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
 /**
  * Created by topema on 25/11/2017.
  */
 
-public class commentContent {
+public class commentContent extends AppCompatActivity implements ConnectResponse {
+
+    Connect con;
+    EditText editText;
+    Button comentar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.comment);
+
+        editText = findViewById(R.id.editText);
+        comentar = findViewById(R.id.commentButton);
+
+
+    }
+
+    @Override
+    public void processFinish(String output, ArrayList<ArrayList<String>> datos) {
+
+    }
 }
