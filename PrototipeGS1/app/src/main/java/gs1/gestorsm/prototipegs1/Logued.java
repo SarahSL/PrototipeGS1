72752e857ceb09cd6a5eda5c24dfefea531f94a1
LACIE\Sarah <sarahsanchezleon@hotmail.com>
2017-11-26 21:34:16 +0000

    cds

--- PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/Logued.java
+++ PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/Logued.java
@@ -7,16 +7,12 @@ import android.support.v7.app.AppCompatActivity;
  * Created by Jorge on 25/11/2017.
  */
 
-//CAMBIAR POR FILTRACATEGORIA.
 public class Logued extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_logued);
-        //EJEMPLO DONDE RECOGEMOS LA VARIABLE GLOBAL QUE TIENE EL NUMERO
-        MySession g = MySession.getInstance();
-        System.out.println(g.getId() + " - USER LOGUED");
+        setContentView(R.layout.logueado);
     }
 }
