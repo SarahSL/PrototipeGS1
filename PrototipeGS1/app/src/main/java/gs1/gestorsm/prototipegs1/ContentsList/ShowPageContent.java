21c5cbdd3db9cb57d206b618ad29b66b7587b4f6
DESKTOP-VQH35FS\Sarah <sarahsanchezleon@hotmail.com>
2017-11-28 14:15:48 +0000

    agregar SERIE in currently listo :)

--- PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/ContentsList/ShowPageContent.java
+++ PrototipeGS1/app/src/main/java/gs1/gestorsm/prototipegs1/ContentsList/ShowPageContent.java
@@ -15,11 +15,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -46,7 +48,8 @@ public class ShowPageContent extends AppCompatActivity implements ConnectRespons
     private SectionsPageAdapter mSectionsPageAdapter;
     ArrayList<ArrayList<String>> datos = new ArrayList<>();
     private ViewPager mViewPager;
-    String idContent;
+    String idContent, idUser;
+    String idCap = "1";
     Connect con;
     Spinner spinner;
     ArrayList<String> addToList = new ArrayList<>();
@@ -62,6 +65,7 @@ public class ShowPageContent extends AppCompatActivity implements ConnectRespons
         Intent intent = getIntent();
         idContent = intent.getStringExtra("aux");
         g.setIdContent_PageContent(idContent);
+        idUser = g.getId();
 
 //TAB
         mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
@@ -106,46 +110,93 @@ public class ShowPageContent extends AppCompatActivity implements ConnectRespons
             nameP.setText(contentDatas.get(2));
             TextView linkP = findViewById(R.id.link_for_platform);
             linkP.setText(contentDatas.get(3));
+
         } else if (paso == 1) {
             setDataForAdd();
-            paso = 2;
             if (addToList.get(0).equals("serie")) {
-                ifSerie();
+                System.out.println("COMPUREBO SI ESTA LA SERIE_ AAAA");
+                paso = 2;
+                ifEstaYserie();
             } else {
-                ifMovie();
+                ifEStaYMovie();
             }
         } else if (paso == 2) {
-
+            System.out.println(datos.size());
+            if (datos.size()==0) {
+                if (addToList.get(0).equals("serie")) {
+                    paso =3;
+                    insertarEnListSerie();
+                } else {
+                    insertarEnListMovie();
+                }
+            } else {
+                Toast.makeText(this, "YA ESTA EN LA LISTA", Toast.LENGTH_SHORT).show();
+            }
         }
+    }
 
+    private void insertarEnListMovie() {
 
     }
 
-    private void ifSerie() {
+    private void insertarEnListSerie() {
         switch (lista) {
             case 1:
+                System.out.println("AAAAAAA" + "--inserto en lista");
+                if (idContent != null) {
+                    con = new Connect();
+                    con.setSql("INSERT INTO watchingList(cod_user,cod_movie,cod_chapter) Values(" + idUser + ",null," + idCap + ")", 1);
+                    con.delegate = this;
+                    con.Connect();
+                }
+                break;
             case 2:
             case 3:
             case 4:
 
         }
+    }
 
+    private void ifEstaYserie() {
+        switch (lista) {
+            case 1:
+                System.out.println("AAAAAAA -- esta y es serie?" + idCap + idContent + idUser);
+                if (idContent != null) {
+                    con = new Connect();
+                    con.setSql("Select watchingList.id_watchingList from watchingList " +
+                            "inner join chapter,user,serie,season,content " +
+                            "where content.id_content=" + idContent +
+                            " and content.id_content = serie.cod_content " +
+                            "and serie.id_serie = season.cod_serie " +
+                            "and season.id_season = chapter.cod_season " +
+                            "and chapter.id_chapter =" + idCap +
+                            " and chapter.id_chapter = watchingList.cod_chapter " +
+                            "and watchingList.cod_user = user.id_user and user.id_user =" + idUser, 0);
+                    con.delegate = this;
+                    con.Connect();
+                }
+                break;
+            case 2:
+            case 3:
+            case 4:
 
-        if (idContent != null) {
-            con = new Connect();
-            con.setSql("", 0);
-            con.delegate = this;
-            con.Connect();
         }
     }
 
-    private void ifMovie() {
-        if (idContent != null) {
-            con = new Connect();
-            con.setSql("", 0);
-            con.delegate = this;
-            con.Connect();
+    private void ifEStaYMovie() {
+        switch (lista) {
+            case 1:
+                if (idContent != null) {
+                    con = new Connect();
+                    con.setSql("", 0);
+                    con.delegate = this;
+                    con.Connect();
+                }
+            case 2:
+            case 3:
+            case 4:
         }
+
     }
 
     private void setDataForAdd() {
@@ -189,14 +240,22 @@ public class ShowPageContent extends AppCompatActivity implements ConnectRespons
         switch (i) {
             case 1: //CW
                 lista = 1;
-                getType();
+                EditText editText = findViewById(R.id.number_chapter);
+                idCap = editText.getText().toString();
                 paso = 1;
+                getType();
                 break;
             case 2: //PTW
+                lista = 2;
+                getType();
                 break;
             case 3: //C
+                lista = 3;
+                getType();
                 break;
             case 4:  //OH
+                lista = 4;
+                getType();
                 break;
         }
     }
